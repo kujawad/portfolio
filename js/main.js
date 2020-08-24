b40a533795114f0d7e53fba66d51208bcf38d40e
kujawad <kujawa.dp@gmail.com>
2020-08-24 15:59:37 +0200

    remove redundant libraries

--- js/main.js
+++ js/main.js
@@ -1,36 +1,29 @@
-// navigation starts here
-$("#toggle").click(function() {
-      $(this).toggleClass('on');
-      $("#resize").toggleClass("active");
+// include header and footer
+function onLoad() {
+    $(function () {
+        $("#navbar").load("navbar.html");
+        $("#footer").load("footer.html");
+    });
+}
+
+function onMenuClick() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+}
+
+$("#toggle").click(function () {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
 });
 
-$("#resize ul li a").click(function() {
-      $(this).toggleClass('on');
-      $("#resize").toggleClass("active");
+$("#resize ul li a").click(function () {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
 });
 
-$(".close-btn").click(function() {
-      $(this).toggleClass('on');
-      $("#resize").toggleClass("active");
+$(".close-btn").click(function () {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
 });
-
-// navigation ends here
-
-// nav animation
-
-TweenMax.from("#brand", 1, {
-      delay: 0.4,
-      y: 10,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-
-TweenMax.staggerFrom("#menu li a", 1, {
-      delay: 0.4,
-      opacity: 0,
-      ease: Expo.easeInOut
-}, 0.1);
-
-// nav animation ends
 
 new WOW().init();
